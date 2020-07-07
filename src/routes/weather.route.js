4874c6994bec31f4972c6dc40f5065a6b1287b64
Manuel Pascual <manupascual90@gmail.com>
2020-07-07 11:53:08 -0300

    Add eslint and prettier configuration

--- src/routes/weather.route.js
+++ src/routes/weather.route.js
@@ -3,10 +3,6 @@ const router = Router();
 const weatherController = require("../controllers/weather.controller");
 const extractCityMiddleware = require("../middlewares/extractCity.mw");
 
-router.get(
-  "/:city?",
-  extractCityMiddleware,
-  weatherController.getCurrentWeather
-);
+router.get("/:city?", extractCityMiddleware, weatherController.getCurrentWeather);
 
 module.exports = router;
