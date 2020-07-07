de6b82c0e9d4a846d6b8e3780f5ee71861b9427b
Manuel Pascual <manupascual90@gmail.com>
2020-07-07 12:10:44 -0300

    Disable eslint on specific line inside server.js file

--- src/server.js
+++ src/server.js
@@ -5,5 +5,6 @@ app.set("port", process.env.PORT || 8080);
 
 // Run server
 app.listen(app.get("port"), () => {
+  // eslint-disable-next-line
   console.log(`Server listening on port ${app.get("port")}...`);
 });
